3b0ebff25b1fb773504708caec4d13fb795dc52e
Thaung Htike Oo <thaunghtikeoo.tho1234@gmail.com>
2022-04-18 23:38:15 +0630

    merging with remote

---
+++ examples/complete/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "aws_oidc_gitlab" {
+  source = "../"
+
+
+  attach_admin_policy     = var.attach_admin_policy
+  create_oidc_provider    = var.create_oidc_provider
+  aws_managed_policy_arns = var.aws_managed_policy_arns
+  gitlab_url              = var.gitlab_url
+  audience                = var.audience
+  match_field             = var.match_field
+  match_value             = var.match_value
+}
