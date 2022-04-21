ee21d8dd04fd26b5f6354348f14651f166016712
thaunghtike-share <87596555+thaunghtike-share@users.noreply.github.com>
2022-04-21 06:51:26 +0630

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -32,14 +32,14 @@ EOT
 
 module "aws_oidc_gitlab" {
   for_each = var.gitlab
-  source = "../../"
+  source   = "../../"
 
-  iam_role_name           = "gitlab_action_oidc_aws"
-  attach_admin_policy     = true
-  create_oidc_provider    = true
-  aws_managed_policy_arns = [aws_iam_policy.deployer.arn]
-  gitlab_url              = "https://gitlab.com"
-  audience                = "https://gitlab.com"
-  match_field             = each.value.match_field
-  match_value             = each.value.match_value
+  iam_role_name        = "gitlab_action_oidc_aws"
+  attach_admin_policy  = true
+  create_oidc_provider = true
+  iam_policy_arns      = [aws_iam_policy.deployer.arn]
+  gitlab_url           = "https://gitlab.com"
+  audience             = "https://gitlab.com"
+  match_field          = each.value.match_field
+  match_value          = each.value.match_value
 }
